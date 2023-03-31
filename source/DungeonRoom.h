3c387de8c8d58a0de9296d8af79423d5f804edbe
L Carver <wifihum@gmail.com>
2023-03-30 23:42:12 -0700

    Add project files.

---
+++ source/DungeonRoom.h
@@ -0,0 +1,42 @@
+//------------------------------------------------------------------------------
+// DungeonRoom.h : class declaration/definition, version 0.1
+// 
+// One room in a game dungeon.
+// All members are public in this initial version 0.1.
+// Each room contains a string description of itself.
+// Each room has a point value, given by member variable points.
+// Add a dungeon rooms to your game by creating DungeonRoom instances.
+// Connect dungeon rooms by setting the pointers.
+//------------------------------------------------------------------------------
+#pragma once
+
+#include <string>
+using std::string;
+
+//------------------------------------------------------------------------------
+// DungeonNode 
+//------------------------------------------------------------------------------
+class DungeonRoom {
+public:
+    // pointers to other rooms
+    DungeonRoom* pNorth;
+    DungeonRoom* pSouth;
+    DungeonRoom* pEast;
+    DungeonRoom* pWest;
+
+    // text description of this room
+    string description;
+    // point value for this room
+    int points;
+
+    // constructor
+    DungeonRoom(const string& _desc, int _points) : 
+        description(_desc), points(_points) {
+
+        pNorth = pSouth = pEast = pWest = nullptr;
+    }
+
+    // destructor #TODO
+    ~DungeonRoom() { }
+};
+
